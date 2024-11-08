d5457f6358c9c8d60c3c0400ec5203daebe5ca02
❥ giulia <giuliamferreira01@gmail.com>
2024-11-08 15:53:03 -0300

    Add files via upload

---
+++ Lista 1/frequency_pt.py
@@ -0,0 +1,11 @@
+# https://github.com/giuliamf
+# Author: Giulia Moura, 200018795
+
+# https://www.dcc.fc.up.pt/~rvr/naulas/tabelasPT/
+
+frequency_portuguese = {
+    'A': 13.9, 'B': 1.0, 'C': 4.4, 'D': 5.4, 'E': 12.2, 'F': 1.0, 'G': 1.2,
+    'H': 0.8, 'I': 6.9, 'J': 0.4, 'K': 0.1, 'L': 2.8, 'M': 4.2, 'N': 5.3,
+    'O': 10.8, 'P': 2.9, 'Q': 0.9, 'R': 6.9, 'S': 7.9, 'T': 4.9, 'U': 4.0,
+    'V': 1.3, 'W': 0.0, 'X': 0.3, 'Y': 0.0, 'Z': 0.4
+}
